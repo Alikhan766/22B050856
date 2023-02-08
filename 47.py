5f9f55f1a97f4bdf2af7679fb8046e1f62bcb7a3
Alikhan766 <121330838+Alikhan766@users.noreply.github.com>
2023-02-08 19:25:28 +0600

    Delete 47.py

--- 47.py
+++
@@ -1,5 +0,0 @@
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
-thislist[1:3] = ["blackcurrant", "watermelon"]
-print(thislist)
-thislist[1:2] = ["blackcurrant", "watermelon"]
-print(thislist)
